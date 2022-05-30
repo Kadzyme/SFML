e71b8588b418058db356cf791ca4b093d30f519d
Dark <darkcitadel228@gmail.com>
2022-05-30 17:38:52 +0300

    WIP Agario

--- Test/Program.cs
+++ Test/Program.cs
@@ -13,249 +13,165 @@ namespace Game
         }
     }
 
-    public struct Player
+    public class Player
     {
-        public RectangleShape playerShape;
+        public CircleShape playerShape;
+
         public Keyboard.Key DownKey;
         public Keyboard.Key UpKey;
-        public int score;
+        public Keyboard.Key LeftKey;
+        public Keyboard.Key RightKey;
 
-        public Player(RectangleShape playerShape, Keyboard.Key DownKey, Keyboard.Key UpKey, int score)
-        {
-            this.playerShape = playerShape;
-            this.DownKey = DownKey;
-            this.UpKey = UpKey;
-            this.score = score;
-        }
-    }
-
-    public class Ball
-    {
-        public CircleShape ballShape;
-        public Vector2f startBallPosition;
-        public Player owner;
-
-        public float startBallSpeed;
-        public float ballSpeedX;
-        public float ballSpeedY;
-        public float normalBallSpeed;
-
-        public void SetNormalSpeed()
-        {
-            normalBallSpeed = startBallSpeed;
-            ballSpeedX = startBallSpeed;
-            ballSpeedY = startBallSpeed;
-        }
-
-        public void SetStartSettings(float startSpeed, float ballRadius, Color ballShapeColor, Vector2f ballPosition)
-        {
-            startBallSpeed = startSpeed;
-            ballShape = new(ballRadius);
-            ballShape.FillColor = ballShapeColor;
-            ballShape.Position = ballPosition;
-            startBallPosition = ballPosition;
-            ballShape.Origin = new Vector2f(ballShape.Radius, ballShape.Radius);
-        }
+        public float currentTimeForRevive = 0;
     }
 
     public class Game
     {
-        public RenderWindow window = new RenderWindow(new VideoMode(1600, 900), "Game");
+        private RenderWindow window = new RenderWindow(new VideoMode(1600, 900), "Game");
 
-        public Player rightPlayer;
-        public Player leftPlayer;
+        private Player rightPlayer = new();
+        private Player leftPlayer = new();
 
-        private CircleShape coin = new CircleShape(30);
+        private List<CircleShape> foodList = new();
+        private List<Player> playerList = new();
 
-        private Text scoreText = new();
+        //private List<CircleShape> listToDraw = new();
 
-        private Ball ball = new();
+        private int timeForRevivePlayer = 5;
 
         private void Init()
         {
             window.SetFramerateLimit(60);
             window.Closed += WindowClosed;
 
-            scoreText.Style = Text.Styles.Bold; 
-            scoreText.Font = new Font("C:\\Windows\\Fonts\\Arial.ttf");
-            scoreText.CharacterSize = 120;
-            scoreText.Position = new Vector2f(window.Size.X / 2 - (scoreText.Scale.X * scoreText.CharacterSize), 0);
+            leftPlayer = SetStartPlayerSettings(leftPlayer, Keyboard.Key.W, Keyboard.Key.S, Keyboard.Key.A, Keyboard.Key.D, Color.Blue);
 
-            leftPlayer.playerShape = new RectangleShape(new Vector2f(25, 120));
-            leftPlayer = SetPlayerSettings(leftPlayer.playerShape.Size.X / 2, leftPlayer, Keyboard.Key.W, Keyboard.Key.S, Color.Blue);
-
-            rightPlayer.playerShape = new RectangleShape(new Vector2f(25, 120));
-            rightPlayer = SetPlayerSettings(window.Size.X - rightPlayer.playerShape.Size.X / 2, rightPlayer, Keyboard.Key.Up, Keyboard.Key.Down, Color.Red);
-
-            coin.FillColor = Color.Yellow;
-            coin.Origin = new Vector2f(coin.Radius, coin.Radius);
-            CoinMove();
-
-            SetNeutralOwnerForBall();
-            ball.SetStartSettings(4f, 25f, Color.Green, new Vector2f(window.Size.X / 2, window.Size.Y / 2));
-            ball.SetNormalSpeed();
+            rightPlayer = SetStartPlayerSettings(rightPlayer, Keyboard.Key.Up, Keyboard.Key.Down, Keyboard.Key.Left, Keyboard.Key.Right, Color.Red);
         }
 
-        private void SetNeutralOwnerForBall()
+        private Player SetStartPlayerSettings(Player player, Keyboard.Key UpKey, Keyboard.Key DownKey, Keyboard.Key LeftKey, Keyboard.Key RightKey, Color playerShapeColor)
         {
-            Player neutralPlayer = new();
-            neutralPlayer.playerShape = new();
-            neutralPlayer.score = 0;
-            neutralPlayer.playerShape.FillColor = Color.Green;
-            ball.owner = neutralPlayer;
-        }
-
-        private Player SetPlayerSettings(float playerPosX, Player player, Keyboard.Key UpKey, Keyboard.Key DownKey, Color playerShapeColor)
-        {
+            player.playerShape = new(30);
             player.DownKey = DownKey;
             player.UpKey = UpKey;
+            player.LeftKey = LeftKey;
+            player.RightKey = RightKey;
             player.playerShape.FillColor = playerShapeColor;
-            player.playerShape.Origin = new Vector2f(player.playerShape.Size.X / 2, player.playerShape.Size.Y / 2);
-            player.playerShape.Position = new Vector2f(playerPosX, player.playerShape.Size.Y / 2);
-            player.score = 0;
+            Random rand = new Random();
+            player.playerShape.Origin = new Vector2f(player.playerShape.Radius, player.playerShape.Radius);
+            player.playerShape.Position = new Vector2f(rand.Next((int)player.playerShape.Radius, (int)window.Size.X - (int)player.playerShape.Radius), rand.Next((int)player.playerShape.Radius, (int)window.Size.Y - (int)player.playerShape.Radius));
+            playerList.Add(player);
             return player;
         }
 
         public void GameLoop()
         {
+            float foodSpawnRate = 1f;
+            Clock time = new();
             Init();
             while (window.IsOpen)
             {
-                PlayerMove(leftPlayer.UpKey, leftPlayer.DownKey, leftPlayer);
-                PlayerMove(rightPlayer.UpKey, rightPlayer.DownKey, rightPlayer);
-                BallMove(ball);
-                SetTextForScore();
+                PlayerMove(leftPlayer);
+                PlayerMove(rightPlayer);
+                if (time.ElapsedTime.AsSeconds() >= foodSpawnRate)
+                {
+                    foodList.Add(SpawnFood());
+                    time.Restart();
+                }
+                foreach (Player player in playerList)
+                {
+                    player.currentTimeForRevive -= time.ElapsedTime.AsSeconds();
+                }
                 Draw();
             }
         }
 
-        private void CoinMove()
+        private void CheckCollisions(Player player)
+        {
+            foreach (CircleShape food in foodList)
+            {
+                if (Collide(player.playerShape, food))
+                {
+                    foodList.Remove(food);
+                    food.Dispose();
+                    player.playerShape.Radius += 0.5f;
+                    player.playerShape.Origin = new Vector2f(player.playerShape.Radius, player.playerShape.Radius);
+                    break;
+                }
+            }
+            foreach (Player otherPlayer in playerList)
+            {
+                if (Collide(player.playerShape, otherPlayer.playerShape))
+                {
+                    if (player.playerShape.Radius > otherPlayer.playerShape.Radius)
+                        EatingPlayer(otherPlayer, player);
+                    else
+                        EatingPlayer(player, otherPlayer);
+                    break;
+                }
+            }
+        }
+
+        private void EatingPlayer(Player playerForDestroy, Player playerForReward)
+        {
+            playerList.Remove(playerForDestroy);
+            playerForDestroy.playerShape.Dispose();
+            playerForDestroy.currentTimeForRevive = timeForRevivePlayer;
+            //playerForReward.playerShape.Radius += playerForDestroy.playerShape.Radius;
+            playerForReward.playerShape.Origin = new Vector2f(playerForReward.playerShape.Radius, playerForReward.playerShape.Radius);
+        }
+
+        private CircleShape SpawnFood()
         {
             Random rand = new Random();
-            int radius = Convert.ToInt32(coin.Radius);
-            int windowSizeX = Convert.ToInt32(window.Size.X);
-            int windowSizeY = Convert.ToInt32(window.Size.Y);
-            coin.Position = new Vector2f(rand.Next(radius * 2, windowSizeX - (radius * 2)), rand.Next(radius, windowSizeY - radius));
-        }
-        
-        private void SetTextForScore()
-        {
-            scoreText.FillColor = Color.Green;
-            scoreText.DisplayedString = $"{leftPlayer.score}:{rightPlayer.score}";
+            CircleShape food = new(15);
+            food.FillColor = new Color((byte)rand.Next(0, 255), (byte)rand.Next(0, 255), (byte)rand.Next(0, 255));
+            food.Position = new Vector2f(rand.Next((int)food.Radius, (int)window.Size.X - (int)food.Radius), rand.Next((int)food.Radius, (int)window.Size.Y - (int)food.Radius));
+            return food;
         }
 
-        private void BallMove(Ball ball)
-        {
-            var ballShape = ball.ballShape;
-            var normalBallSpeed = ball.normalBallSpeed;
-            var ballSpeedX = ball.ballSpeedX;
-            var ballSpeedY = ball.ballSpeedY;
+        private bool Collide(CircleShape firstCircle, CircleShape secondCircle)
+            => secondCircle.Position.X + secondCircle.Radius >= firstCircle.Position.X - firstCircle.Radius
+            && secondCircle.Position.X - secondCircle.Radius <= firstCircle.Position.X + firstCircle.Radius
+            && secondCircle.Position.Y - secondCircle.Radius <= firstCircle.Position.Y + firstCircle.Radius
+            && secondCircle.Position.Y + secondCircle.Radius >= firstCircle.Position.Y - firstCircle.Radius;
 
-            if (ballShape.Position.X + ballShape.Radius >= window.Size.X || ballShape.Position.X - ballShape.Radius <= 0)
-            {
-                normalBallSpeed = ball.startBallSpeed;
-                if (ballSpeedX > 0)
-                {
-                    ballSpeedX = normalBallSpeed;
-                }
-                else
-                {
-                    ballSpeedX = -normalBallSpeed;
-                }
-                if (ballSpeedY > 0)
-                    ballSpeedY = normalBallSpeed;
-                else
-                    ballSpeedY = -normalBallSpeed;
-                if (ball.owner.playerShape == leftPlayer.playerShape)
-                    leftPlayer.score++;
-                else if (ball.owner.playerShape == rightPlayer.playerShape)
-                    rightPlayer.score++;
-                SetNeutralOwnerForBall();
-                ballShape.Position = ball.startBallPosition;
-            }
-            if (BallTouchPlayer(rightPlayer.playerShape, ball.ballShape))
-            {
-                ballSpeedX = -normalBallSpeed;
-                ball.owner = rightPlayer;
-            }
-            else if (BallTouchPlayer(leftPlayer.playerShape, ball.ballShape))
-            {
-                ballSpeedX = normalBallSpeed;
-                ball.owner = leftPlayer;
-            }
-            if (BallTouchCoin(coin, ball.ballShape))
-            {
-                if (ball.owner.playerShape == leftPlayer.playerShape)
-                    leftPlayer.score++;                
-                else if (ball.owner.playerShape == rightPlayer.playerShape)
-                    rightPlayer.score++;
-                CoinMove();
-            }
-            ball.ballShape.FillColor = ball.owner.playerShape.FillColor;
-            if (ballShape.Position.Y - ballShape.Radius <= 0)
-            {
-                ballSpeedY = normalBallSpeed;
-            }
-            else if (ballShape.Position.Y + ballShape.Radius >= window.Size.Y)
-            {
-                ballSpeedY = -normalBallSpeed;
-            }
-            ballShape.Position += new Vector2f(ballSpeedX, ballSpeedY);
-
-            ball.normalBallSpeed = BallSpeedIncrease(normalBallSpeed);
-            ball.ballSpeedX = BallSpeedIncrease(ballSpeedX);
-            ball.ballSpeedY = BallSpeedIncrease(ballSpeedY);
-        }
-
-        private bool BallTouchPlayer (RectangleShape player, CircleShape ball)
-            => ball.Position.X + ball.Radius >= player.Position.X - player.Size.X / 2
-            && ball.Position.X - ball.Radius <= player.Position.X + player.Size.X / 2
-            && ball.Position.Y - ball.Radius <= player.Position.Y + player.Size.Y / 2
-            && ball.Position.Y + ball.Radius >= player.Position.Y - player.Size.Y / 2;
-
-        private bool BallTouchCoin(CircleShape coin, CircleShape ball)
-            => ball.Position.X + ball.Radius >= coin.Position.X - coin.Radius
-            && ball.Position.X - ball.Radius <= coin.Position.X + coin.Radius
-            && ball.Position.Y - ball.Radius <= coin.Position.Y + coin.Radius
-            && ball.Position.Y + ball.Radius >= coin.Position.Y - coin.Radius;
-
-        private float BallSpeedIncrease(float speed)
-        {
-            float speedIncrease = 0.004f;
-            Random rand = new Random();
-            if (speed == 0)
-                speed += speedIncrease * rand.Next(-1, 2);
-            else if (speed > 0)
-                speed += speedIncrease;
-            else
-                speed -= speedIncrease;
-            return speed;
-        }
-
-        private void PlayerMove(Keyboard.Key UpKey, Keyboard.Key DownKey, Player player)
+        private void PlayerMove(Player player)
         {
             float playerSpeed = 7;
-            float movePlayer = 0;
-            if (Keyboard.IsKeyPressed(DownKey) && player.playerShape.Position.Y + player.playerShape.Size.Y / 2 < window.Size.Y)
+            Vector2f movePlayer = new(0,0);
+            if (Keyboard.IsKeyPressed(player.DownKey) && player.playerShape.Position.Y + player.playerShape.Radius < window.Size.Y)
             {
-                movePlayer += playerSpeed;
+                movePlayer.Y += playerSpeed;
             }
-            else if (Keyboard.IsKeyPressed(UpKey) && player.playerShape.Position.Y - player.playerShape.Size.Y / 2 > 0)
+            else if (Keyboard.IsKeyPressed(player.UpKey) && player.playerShape.Position.Y - player.playerShape.Radius > 0)
             {
-                movePlayer -= playerSpeed;
+                movePlayer.Y -= playerSpeed;
             }
-            player.playerShape.Position += new Vector2f(0, movePlayer);
+            if (Keyboard.IsKeyPressed(player.RightKey) && player.playerShape.Position.X + player.playerShape.Radius < window.Size.X)
+            {
+                movePlayer.X += playerSpeed;
+            }
+            else if (Keyboard.IsKeyPressed(player.LeftKey) && player.playerShape.Position.X - player.playerShape.Radius > 0)
+            {
+                movePlayer.X -= playerSpeed;
+            }
+            player.playerShape.Position += movePlayer;
+            CheckCollisions(player);
         }
 
         private void Draw()
         {
             window.DispatchEvents();
-            window.Clear();
-            window.Draw(scoreText);
-            window.Draw(coin);
-            window.Draw(ball.ballShape);
-            window.Draw(leftPlayer.playerShape);
-            window.Draw(rightPlayer.playerShape);
+            window.Clear(Color.Cyan);
+            foreach (CircleShape food in foodList)
+            {
+                window.Draw(food);
+            }
+            foreach (Player player in playerList)
+            {
+                window.Draw(player.playerShape);
+            }
             window.Display();
         }
 
