062fe9dcc742ec7403209866d2d21d9367015ceb
Dark <darkcitadel228@gmail.com>
2022-05-31 17:55:15 +0300

    Working Agario(without bots and victory)

--- Test/Program.cs
+++ Test/Program.cs
@@ -22,6 +22,7 @@ namespace Game
         public Keyboard.Key LeftKey;
         public Keyboard.Key RightKey;
 
+        public bool isAlive;
         public float currentTimeForRevive = 0;
     }
 
@@ -34,8 +35,6 @@ namespace Game
 
         private List<CircleShape> foodList = new();
         private List<Player> playerList = new();
-
-        //private List<CircleShape> listToDraw = new();
 
         private int timeForRevivePlayer = 5;
 
@@ -51,7 +50,7 @@ namespace Game
 
         private Player SetStartPlayerSettings(Player player, Keyboard.Key UpKey, Keyboard.Key DownKey, Keyboard.Key LeftKey, Keyboard.Key RightKey, Color playerShapeColor)
         {
-            player.playerShape = new(30);
+            player.playerShape = new(25);
             player.DownKey = DownKey;
             player.UpKey = UpKey;
             player.LeftKey = LeftKey;
@@ -61,28 +60,84 @@ namespace Game
             player.playerShape.Origin = new Vector2f(player.playerShape.Radius, player.playerShape.Radius);
             player.playerShape.Position = new Vector2f(rand.Next((int)player.playerShape.Radius, (int)window.Size.X - (int)player.playerShape.Radius), rand.Next((int)player.playerShape.Radius, (int)window.Size.Y - (int)player.playerShape.Radius));
             playerList.Add(player);
+            player.isAlive = true;
             return player;
         }
 
         public void GameLoop()
         {
             float foodSpawnRate = 1f;
+            float currentTimeToSpawnFood = foodSpawnRate;
             Clock time = new();
             Init();
             while (window.IsOpen)
             {
                 PlayerMove(leftPlayer);
                 PlayerMove(rightPlayer);
-                if (time.ElapsedTime.AsSeconds() >= foodSpawnRate)
+                CheckTimeForPlayerRespawn(time.ElapsedTime.AsSeconds());
+                if (currentTimeToSpawnFood <= 0)
                 {
+                    currentTimeToSpawnFood = foodSpawnRate;
                     foodList.Add(SpawnFood());
-                    time.Restart();
                 }
-                foreach (Player player in playerList)
-                {
-                    player.currentTimeForRevive -= time.ElapsedTime.AsSeconds();
-                }
+                currentTimeToSpawnFood -= time.ElapsedTime.AsSeconds();
+                time.Restart();
                 Draw();
+            }
+        }
+
+        private void CheckTimeForPlayerRespawn(float time)
+        {
+            foreach (Player player in playerList)
+            {
+                player.currentTimeForRevive -= time;
+                if (player.currentTimeForRevive <= 0 && !player.isAlive)
+                {
+                    Random rand = new();
+                    player.playerShape.Position = new Vector2f(rand.Next((int)player.playerShape.Radius, (int)window.Size.X - (int)player.playerShape.Radius), rand.Next((int)player.playerShape.Radius, (int)window.Size.Y - (int)player.playerShape.Radius));
+                    player.isAlive = true;
+                }
+            }
+        }
+
+        private CircleShape SpawnFood()
+        {
+            Random rand = new Random();
+            CircleShape food = new(15);
+            food.Origin = new Vector2f(food.Radius, food.Radius);
+            food.FillColor = new Color((byte)rand.Next(0, 255), (byte)rand.Next(0, 255), (byte)rand.Next(0, 255));
+            food.Position = new Vector2f(rand.Next((int)food.Radius, (int)window.Size.X - (int)food.Radius), rand.Next((int)food.Radius, (int)window.Size.Y - (int)food.Radius));
+            return food;
+        }
+
+        private bool Collide(CircleShape firstCircle, CircleShape secondCircle)
+            => Math.Abs(secondCircle.Position.X - firstCircle.Position.X) <= secondCircle.Radius + firstCircle.Radius 
+            && Math.Abs(secondCircle.Position.Y - firstCircle.Position.Y) <= secondCircle.Radius + firstCircle.Radius;
+
+        private void PlayerMove(Player player)
+        {
+            if (player.isAlive)
+            {
+                float playerSpeed = 7;
+                Vector2f movePlayer = new(0, 0);
+                if (Keyboard.IsKeyPressed(player.DownKey) && player.playerShape.Position.Y + player.playerShape.Radius < window.Size.Y)
+                {
+                    movePlayer.Y += playerSpeed;
+                }
+                else if (Keyboard.IsKeyPressed(player.UpKey) && player.playerShape.Position.Y - player.playerShape.Radius > 0)
+                {
+                    movePlayer.Y -= playerSpeed;
+                }
+                if (Keyboard.IsKeyPressed(player.RightKey) && player.playerShape.Position.X + player.playerShape.Radius < window.Size.X)
+                {
+                    movePlayer.X += playerSpeed;
+                }
+                else if (Keyboard.IsKeyPressed(player.LeftKey) && player.playerShape.Position.X - player.playerShape.Radius > 0)
+                {
+                    movePlayer.X -= playerSpeed;
+                }
+                player.playerShape.Position += movePlayer;
+                CheckCollisions(player);
             }
         }
 
@@ -101,7 +156,8 @@ namespace Game
             }
             foreach (Player otherPlayer in playerList)
             {
-                if (Collide(player.playerShape, otherPlayer.playerShape))
+                if (Collide(player.playerShape, otherPlayer.playerShape) && otherPlayer.isAlive
+                    && player.playerShape.Radius != otherPlayer.playerShape.Radius)
                 {
                     if (player.playerShape.Radius > otherPlayer.playerShape.Radius)
                         EatingPlayer(otherPlayer, player);
@@ -114,50 +170,10 @@ namespace Game
 
         private void EatingPlayer(Player playerForDestroy, Player playerForReward)
         {
-            playerList.Remove(playerForDestroy);
-            playerForDestroy.playerShape.Dispose();
+            playerForDestroy.isAlive = false;
             playerForDestroy.currentTimeForRevive = timeForRevivePlayer;
-            //playerForReward.playerShape.Radius += playerForDestroy.playerShape.Radius;
+            playerForReward.playerShape.Radius += playerForDestroy.playerShape.Radius;
             playerForReward.playerShape.Origin = new Vector2f(playerForReward.playerShape.Radius, playerForReward.playerShape.Radius);
-        }
-
-        private CircleShape SpawnFood()
-        {
-            Random rand = new Random();
-            CircleShape food = new(15);
-            food.FillColor = new Color((byte)rand.Next(0, 255), (byte)rand.Next(0, 255), (byte)rand.Next(0, 255));
-            food.Position = new Vector2f(rand.Next((int)food.Radius, (int)window.Size.X - (int)food.Radius), rand.Next((int)food.Radius, (int)window.Size.Y - (int)food.Radius));
-            return food;
-        }
-
-        private bool Collide(CircleShape firstCircle, CircleShape secondCircle)
-            => secondCircle.Position.X + secondCircle.Radius >= firstCircle.Position.X - firstCircle.Radius
-            && secondCircle.Position.X - secondCircle.Radius <= firstCircle.Position.X + firstCircle.Radius
-            && secondCircle.Position.Y - secondCircle.Radius <= firstCircle.Position.Y + firstCircle.Radius
-            && secondCircle.Position.Y + secondCircle.Radius >= firstCircle.Position.Y - firstCircle.Radius;
-
-        private void PlayerMove(Player player)
-        {
-            float playerSpeed = 7;
-            Vector2f movePlayer = new(0,0);
-            if (Keyboard.IsKeyPressed(player.DownKey) && player.playerShape.Position.Y + player.playerShape.Radius < window.Size.Y)
-            {
-                movePlayer.Y += playerSpeed;
-            }
-            else if (Keyboard.IsKeyPressed(player.UpKey) && player.playerShape.Position.Y - player.playerShape.Radius > 0)
-            {
-                movePlayer.Y -= playerSpeed;
-            }
-            if (Keyboard.IsKeyPressed(player.RightKey) && player.playerShape.Position.X + player.playerShape.Radius < window.Size.X)
-            {
-                movePlayer.X += playerSpeed;
-            }
-            else if (Keyboard.IsKeyPressed(player.LeftKey) && player.playerShape.Position.X - player.playerShape.Radius > 0)
-            {
-                movePlayer.X -= playerSpeed;
-            }
-            player.playerShape.Position += movePlayer;
-            CheckCollisions(player);
         }
 
         private void Draw()
@@ -170,7 +186,8 @@ namespace Game
             }
             foreach (Player player in playerList)
             {
-                window.Draw(player.playerShape);
+                if (player.isAlive)
+                    window.Draw(player.playerShape);
             }
             window.Display();
         }
